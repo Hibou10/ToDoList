ca0fe4ea329ce739ce3f13424d9f2a4f3c0060a8
Hicham-stud <hichamboulaayadde@gmail.com>
2024-09-03 21:07:57 +0200

    add functions

--- app.js
+++ app.js
@@ -10,5 +10,34 @@ function addTask(){
           let li = document.createElement("li");
           li.innerHTML = inputBox.value;
           listContainer.appendChild(li);
+          let span = document.createElement("span");
+          span.innerHTML = "\u00d7";
+          li.appendChild(span);
     }
-}
+    // clear input field after adding text
+    inputBox.value = "";
+    saveData();
+} 
+    //
+    listContainer.addEventListener("click", function(e){
+        if (e.target.tagName === "LI") {
+            e.target.classList.toggle("checked");
+            saveData();
+        } 
+        else if (e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+    }, false);
+
+    // store changes when refreshing the page
+    // if not stored, dakchi li dkhelna f input field w li t marka ghadi yemchi
+    function saveData(){
+        localStorage.setItem("data", listContainer.innerHTML);
+    }
+
+    // display this data when we refresh or open browser again
+    function showTask(){
+        listContainer.innerHTML = localStorage.getItem("data");
+    }
+    showTask();
