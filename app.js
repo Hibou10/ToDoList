9a775a930103c04fc4cba888ad44ae1a780b2caf
Boulaayad <h.boulaayad@mvgm.com>
2024-09-03 11:24:34 +0200

    AddTask function added

---
+++ app.js
@@ -0,0 +1,14 @@
+// Create Variable for the element in HTML
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if (inputBox.value === '') {
+        alert("You have to write something!");
+    } 
+    else {
+          let li = document.createElement("li");
+          li.innerHTML = inputBox.value;
+          listContainer.appendChild(li);
+    }
+}
